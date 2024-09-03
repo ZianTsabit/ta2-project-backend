873b7e6130e172102f763060487e4adcb19eb1fc
ZianTsabit <13520165@std.stei.itb.ac.id>
2024-09-03 19:25:04 +0700

    description of a primary key algorithm

--- find_primary_key_algo.py
+++ find_primary_key_algo.py
@@ -1,1 +1,12 @@
 # algorithm for find a primary key
+# so the algorithm will list all the available field in a collection
+# and then deterimine if a field or a combination of a fields can 
+# uniquely identify the data
+# TODO: program to count or determine the value of uniqueness of a field
+# or a combination of a field
+
+# field or combination of a fields that can unqiuely identify the data
+# will be count as a candidate key
+# primary key will be chosen from the candidate key
+# primary key will be the shortest candidate key and also appear on other collection
+
