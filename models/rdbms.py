166ba58742745e60199837a24595402f5210f424
ZianTsabit <13520165@std.stei.itb.ac.id>
2024-10-08 23:43:45 +0700

    fix model

--- models/rdbms.py
+++ models/rdbms.py
@@ -1,3 +1,4 @@
+from psycopg2 import connect
 from pydantic import BaseModel
 
 
@@ -7,3 +8,28 @@ class Rdbms(BaseModel):
     db: str
     username: str
     password: str
+
+    def create_connection(cls) -> connect:
+
+        connection = connect(
+            host=cls.host,
+            port=cls.port,
+            database=cls.db,
+            user=cls.username,
+            password=cls.password
+        )
+
+        return connection
+
+    def test_connection(cls) -> bool:
+        connection = None
+        try:
+            connection = cls.create_connection()
+            cursor = connection.cursor()
+            cursor.execute("SELECT 1")
+            cursor.close()
+            connection.close()
+            return True
+        
+        except OperationalError as e:
+            return False
--- routers/rdbms.py
+++ routers/rdbms.py
@@ -4,6 +4,7 @@ from fastapi.responses import JSONResponse
 
 from models.mysql import MySQL
 from models.postgresql import PostgreSQL
+from models.rdbms import Rdbms
 
 router = APIRouter(
     prefix="/api/rdbms",
@@ -18,3 +19,45 @@ async def root_rdbms():
         content={"message": "RDBMS Routers"},
         status_code=status.HTTP_200_OK
     )
+
+
+@router.post("/test-connection")
+async def test_connection(rdbms_type: str, rdbms: Rdbms):
+    connection_status = False
+    if rdbms_type == 'postgresql':
+        postgresql = PostgreSQL(
+            host=rdbms.host,
+            port=rdbms.port,
+            db=rdbms.db,
+            username=rdbms.username,
+            password=rdbms.password
+        )
+        connection_status = postgresql.test_connection()
+
+    elif rdbms_type == 'mysql':
+        mysql = MySQL(
+            host=rdbms.host,
+            port=rdbms.port,
+            db=rdbms.db,
+            username=rdbms.username,
+            password=rdbms.password
+        )
+        connection_status = mysql.test_connection()
+
+    if connection_status:
+        return JSONResponse(
+            content={
+                "status": connection_status,
+                "message": "connection success"
+            },
+            status_code=status.HTTP_200_OK
+        )
+
+    else:
+        return JSONResponse(
+            content={
+                "status": connection_status,
+                "message": "connection failed"
+            },
+            status_code=status.HTTP_200_OK
+        )
