a44613e7c29e6a0c8bb72177edc3c98a85a227f8
ZianTsabit <13520165@std.stei.itb.ac.id>
2024-11-03 19:36:48 +0700

    update function

--- models/rdbms/postgresql.py
+++ models/rdbms/postgresql.py
@@ -219,7 +219,7 @@ for c in cardinalities:
                     unique=attr.unique
                 )
 
-                dest_rel.foreign_key = foreign_key_attr
+                dest_rel.foreign_key.append(foreign_key_attr)
 
             dest_rel.attributes.append(attr)
 
@@ -297,10 +297,93 @@ for c in cardinalities:
 
         dest_rel.attributes.append(foreign_key)
 
-        dest_rel.foreign_key = foreign_key
+        dest_rel.foreign_key.append(foreign_key)
 
     elif cardinality_type == CardinalitiesType.MANY_TO_MANY:
-        pass
+
+        primary_key_source = mongo.get_primary_key(source_coll)
+
+        for f in source:
+
+            if f.name != dest_coll:
+
+                attr = Attribute(
+                    name=f.name,
+                    data_type=postgresql.data_type_mapping(f.data_type),
+                    not_null=f.not_null,
+                    unique=f.unique
+                )
+
+                if primary_key_source is not None and attr.name == primary_key_source:
+                    source_rel.primary_key = attr
+
+                source_rel.attributes.append(attr)
+
+        if primary_key_source is None:
+            primary_key_attr = Attribute(
+                name="id",
+                data_type=PsqlType.SERIAL,
+                not_null=True,
+                unique=True
+            )
+            source_rel.primary_key = primary_key_attr
+            source_rel.attributes.append(primary_key_attr)
+
+        primary_key_dest = mongo.get_primary_key(dest_coll)
+
+        for f in dest:
+            attr = Attribute(
+                name=f.name,
+                data_type=postgresql.data_type_mapping(f.data_type),
+                not_null=f.not_null,
+                unique=f.unique
+            )
+
+            if primary_key_dest is not None and attr.name == primary_key_dest:
+                dest_rel.primary_key = attr
+
+            dest_rel.attributes.append(attr)
+
+        if primary_key_dest is None:
+            primary_key_attr = Attribute(
+                name="id",
+                data_type=PsqlType.SERIAL,
+                not_null=True,
+                unique=True
+            )
+            dest_rel.primary_key = primary_key_attr
+            dest_rel.attributes.append(primary_key_attr)
+
+        new_relation = Relation(
+            name=f"{source_coll}.{dest_coll}"
+        )
+        new_relation.relations.append(
+            Attribute(
+                name=f"{source_rel.name}.{source_rel.primary_key.name}",
+                data_type=source_rel.primary_key.data_type,
+                not_null=source_rel.primary_key.not_null,
+                unique=source_rel.primary_key.unique
+            )
+        )
+
+        new_relation.relations.append(
+            Attribute(
+                name=f"{dest_rel.name}.{dest_rel.primary_key.name}",
+                data_type=dest_rel.primary_key.data_type,
+                not_null=dest_rel.primary_key.not_null,
+                unique=dest_rel.primary_key.unique
+            )
+        )
+
+        new_relation.primary_key = Attribute(
+            name=f"{source_rel.name}.{source_rel.primary_key.name}, {dest_rel.name}.{dest_rel.primary_key.name}",
+            data_type=PsqlType.NULL,
+            not_null=True,
+            unique=True
+        )
+
+        for attr in new_relation.relations:
+            new_relation.foreign_key.append(attr)
 
     # TODO: check duplicate name relation
 
--- models/rdbms/relation.py
+++ models/rdbms/relation.py
@@ -9,14 +9,14 @@ class Relation(BaseModel):
     name: str
     attributes: List[Attribute] = []
     primary_key: Attribute = None
-    foreign_key: Attribute = None
+    foreign_key: List[Attribute] = []
 
     def to_dict(cls):
         return {
             "name": cls.name,
             "attributes": [attr.to_dict() for attr in cls.attributes],
             "primary_key": cls.primary_key.to_dict(),
-            "foreign_key": cls.foreign_key.to_dict() if cls.foreign_key else None
+            "foreign_key": [attr.to_dict() for attr in cls.foreign_key]
         }
 
 # TODO: when generate ddl please process relation that has no foreign key first
